724c276c24ab7465e3a2e3f6a5bf77ea524092ac
mmshooreshi <mmshooreshi@gmail.com>
2024-09-06 21:30:19 +0330

    init

---
+++ pages/sample.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const { t } = useI18n()
+
+definePageMeta({
+  layout: 'centercard',
+})
+
+useHead({
+  title: `${t('global.sample')} • Norder`,
+})
+</script>
+
+<template>
+  <SampleComponent />
+</template>
